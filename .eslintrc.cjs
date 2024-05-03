e615e868915837c29319526b009425caf4943d9e
Dina Gomes <74661038+dinapgomes@users.noreply.github.com>
2024-05-03 10:02:15 +0000

    Adicionando verificação no Eslint de código duplicado

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,6 @@ module.exports = {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     "react/prop-types": "off",
     "no-warning-comments": "off",
-    "no-dupe-keys": "error" // Adicionando verificação de chaves duplicadas em objetos literais
+    "no-dupe-keys": "warn" // Adicionando verificação de chaves duplicadas em objetos literais
   },
 };
