03cd7c8149cdb03b217400db547ec844f58b617c
Dina Gomes <74661038+dinapgomes@users.noreply.github.com>
2024-06-21 09:45:31 +0000

    :wrench:	chore: adicionando regra no eslint de pacote duplicado no-duplicate-imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,12 +10,16 @@ module.exports = {
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh", "security"],
+  plugins: ["react-refresh", "security", "import"],
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     "react/prop-types": "off",
     "no-warning-comments": "off",
     "security/detect-non-literal-regexp": "warn",
     "security/detect-buffer-noassert": "warn",
+    "import/no-duplicates": "warn",
+    "react/no-typos": "warn",
+    "no-duplicate-case": "warn",
+    
   },
 };
